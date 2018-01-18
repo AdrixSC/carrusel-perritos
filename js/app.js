3760ceee9632495565ec697442a5479ecb71315f
AdrixSC <adriana.be@outlook.com>
2018-01-17 18:40:17 -0600

    funciones completas, falta revisar estilos de imagen

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-$(documnet).ready(loadPage);
+$(document).ready(function (){
+  loadPage();
+});
 
 var target = 0;
 
@@ -14,28 +16,50 @@ var loadPage = function() {
     $next.click(nextImage);
 }
 
-var changeImage = function() {
-    target = parseInt($(this).data("target"));
+//funcion para carlar la imagen con su data
+var changeImage = function() { //cargar imagen
+    target = $(this).data("target");
     showImage(target);
+    changeColorButton(target);
+    previousImage(target);
+    nextImage(target);
 };
 
-var previousImage = function(e) {
-    e.preventDefault();
-    target = target - 1;
-    target(target < 0) ? 6 : target;
-    showImage(target);
-};
-
-var nextImage = function(e) {
-    e.preventDefault();
-    target = target + 1;
-    target = (target > 6) ? 0 : target;
-    showImage(target);
-}
-
-var showImage = function(target) {
+//funcion para cambiar la imagen
+var showImage = function(target) { // changeImage Silvana
     var $lastSlide = $("div.active");
-    var $slide = $("div[data - slide = '" + target + "']");
+    var $slide = $("div[data-slide = '" + target + "']");
     $lastSlide.removeClass("active");
     $slide.addClass("active");
 };
+
+//funcion para cambiar el color de boton activo
+var changeColorButton = function (target) {
+    var $actualButton = $("button.active");
+    console.log($actualButton);
+    var $newButton = $("button[data-target='" + target +"']");
+    $actualButton.removeClass("active");
+    $newButton.addClass("active");
+}
+
+
+//funciones para botones de siguiente y anterior
+
+var previousImage = function(e) {
+  if (e!=null) {
+    e.preventDefault();
+    target = target - 1;
+    target = (target < 0) ? 6 : target;
+    showImage(target);
+    changeColorButton(target);
+};
+
+var nextImage = function(e) {
+    if (e!=null) {
+      e.preventDefault();
+    }
+    target = target + 1;
+    target = (target > 6) ? 0 : target;
+    showImage(target);
+    changeColorButton(target);
+}
